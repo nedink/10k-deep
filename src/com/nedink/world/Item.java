4c8c2a1425c7088dafd117fd7966dbd59c38747c
David Bryant <david@ip0af5216a.int.uvm.edu>
2018-07-17 01:33:02 -0400

    add weapon + item classes

---
+++ src/com/nedink/world/Item.java
@@ -0,0 +1,22 @@
+package com.nedink.world;
+
+public class Item {
+    private float weight;
+    private float volume;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
+}
--- src/com/nedink/world/Player.java
+++ src/com/nedink/world/Player.java
@@ -1,5 +1,65 @@
 package com.nedink.world;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
+    private int level;
+    private int pointsToNextLevel;
+    private int levelProgress;
+
+    public List<Item> inventory;
+    private float inventoryMaxVolume;
+    private float inventoryMaxWeight;
+
+    public Weapon[] weaponSlots;
+    private int weaponSlotsAvailable;
+
+    Player() {
+        level = 0;
+        pointsToNextLevel = 5;
+        levelProgress = 0;
+        inventoryMaxVolume = 4.0f;
+        inventoryMaxWeight = 12.0f;
+        inventory = new ArrayList<>();
+
+        weaponSlotsAvailable = 1;
+        weaponSlots = new Weapon[]{};
+
+    }
+
+    public int acquire(Item item) {
+        if (getInventoryVolume() + item.getVolume() > inventoryMaxVolume)
+            return 1;
+
+        if (getInventoryWeight() + item.getWeight() > inventoryMaxWeight)
+            return 2;
+
+        this.inventory.add(item);
+
+        return 0;
+    }
+
+    public float getInventoryWeight() {
+        float weight = 0f;
+
+        for (Item item : inventory)
+            weight += item.getWeight();
+
+        return weight;
+    }
+
+    public float getInventoryVolume() {
+        float weight = 0f;
+
+        for (Item item : inventory)
+            weight += item.getWeight();
+
+        return weight;
+    }
+
+    private static class Inventory {
+
+    }
 }
