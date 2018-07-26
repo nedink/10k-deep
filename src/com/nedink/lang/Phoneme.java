71d3ed3a225988a936e0b203ecc73416b0a191a1
David Bryant <nedink@gmail.com>
2018-07-26 17:30:27 -0400

    feat(lang): phoneme

--- src/com/nedink/lang/Phoneme.java
+++ src/com/nedink/lang/Phoneme.java
@@ -2,11 +2,11 @@ package com.nedink.lang;
 
 public enum Phoneme {
 
-    A("a", Phoneme.B),
-    E("e", Phoneme.B),
-    I("i", Phoneme.B),
-    O("o", Phoneme.B),
-    U("u", Phoneme.B),
+    I("i"),
+    E("e"),
+    A("a"),
+    O("o"),
+    U("u"),
 
     B("b"),
     C("c"),
@@ -20,7 +20,6 @@ public enum Phoneme {
     M("m"),
     N("n"),
     P("p"),
-    Q("q"),
     R("r"),
     S("s"),
     T("t"),
@@ -31,7 +30,29 @@ public enum Phoneme {
     Z("z")
     ;
 
-    Phoneme(String string, Phoneme... connectors) {
+    static {
+        I.connectors = new Phoneme[]{null, B, C, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, X, Y, Z};
+        E.connectors = new Phoneme[]{null, B, C, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, X, Y, Z};
+        A.connectors = new Phoneme[]{null, B, C, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, X, Y, Z};
+        O.connectors = new Phoneme[]{null, B, C, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, X, Y, Z};
+        U.connectors = new Phoneme[]{null, B, C, D, F, G, H, J, K, L, M, N, P, R, S, T, V, W, X, Y, Z};
 
+        B.connectors = new Phoneme[]{null, I, E, A, O, U};
+        C.connectors = new Phoneme[]{null, I, E, A, O, U, H, L, R, T};
+        D.connectors = new Phoneme[]{null, I, E, A, O, U, R, W};
+        F.connectors = new Phoneme[]{null, I, E, A, O, U, L, R, W};
+        G.connectors = new Phoneme[]{null, I, E, A, O, U, H, L, R, W};
+        H.connectors = new Phoneme[]{null, I, E, A, O, U};
+        J.connectors = new Phoneme[]{null, I, E, A, O, U};
+        K.connectors = new Phoneme[]{null, I, E, A, O, U, H, L, R, W};
+        L.connectors = new Phoneme[]{null, I, E, A, O, U, H, L, R, W};
+    }
+
+    private Phoneme[] connectors;
+    private Phoneme[] connectorsAsLead;
+    private String string;
+
+    Phoneme(String string) {
+        this.string = string;
     }
 }
